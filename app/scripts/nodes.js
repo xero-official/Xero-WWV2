6fba9f1a74360fe38a9de88feb13888478365efe
Ethan van Ballegooyen <ethanvb55@gmail.com>
2019-09-08 15:00:29 +0200

    Updating explorer links

--- app/scripts/nodes.js
+++ app/scripts/nodes.js
@@ -51,8 +51,8 @@ nodes.customNodeObj = {
 nodes.nodeList = {
   xero: {
     name: "XERO",
-    blockExplorerTX: "https://explorer.xerom.org/tx/[[txHash]]",
-    blockExplorerAddr: "https://explorer.xerom.org/addr/[[address]]",
+    blockExplorerTX: "https://explorer.xerom.org/tx/[[txHash]]/internal_transactions",
+    blockExplorerAddr: "https://explorer.xerom.org/address/[[address]]/transactions",
     type: nodes.nodeTypes.XERO,
     eip155: true,
     chainId: 1313500,
